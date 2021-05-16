b3317ece97b659aa4bcef0df374f330e72e7d453
Aline Dubois <juillet.aline@gmail.com>
2021-05-16 21:33:30 +0200

    application Breaking Bad

--- breaking-bad-cards/src/components/Character.js
+++ breaking-bad-cards/src/components/Character.js
@@ -1,1 +1,53 @@
 // https://www.breakingbadapi.com/api/characters/{id}
+import "./Character.css";
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    IconButton,
+    makeStyles,
+    Typography
+} from "@material-ui/core";
+import {Share} from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 300,
+    },
+    media: {
+        width: '300px',
+        height: '400px',
+    }
+}));
+
+export const Character = ({name, birthday, img, status, nickname, portrayed}) => {
+
+    const classes=useStyles();
+
+    return (
+        <Card className={classes.root}>
+            <CardHeader
+                title={portrayed + " as " + name}
+                subheader={birthday}
+            />
+            <CardMedia
+                className={classes.media}
+                image={img}
+                title={name}
+            />
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {nickname}, {status}
+
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="share">
+                    <Share/>
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
--- breaking-bad-cards/src/components/CharacterList.js
+++ breaking-bad-cards/src/components/CharacterList.js
@@ -1,1 +1,43 @@
 // https://www.breakingbadapi.com/api/characters
+import "./CharacterList.css";
+import {useEffect, useState} from "react";
+import {CircularProgress} from "@material-ui/core";
+import {Character} from "./Character";
+
+
+export const CharacterList = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetch("https://www.breakingbadapi.com/api/characters")
+            .then((res) => res.json())
+            .then((res) => setCharacters(res))
+            .catch(err => console.error(err))
+            .finally(() => setIsLoading(false));
+    }, [])
+
+    return (
+
+        <div className="characterList">
+            <h1 className="BB-casting">Breaking Bad</h1>
+            <div className="CharacterList">
+                {isLoading === true ? <CircularProgress/> :
+                    characters.map((character, index) =>
+                        <Character
+                            key={index}
+                            name={character.name}
+                            img={character.img}
+                            birthday={character.birthday}
+                            nickname={character.nickname}
+                            portrayed={character.portrayed}
+                            status={character.status}
+                        />
+                    )
+                }
+            </div>
+
+        </div>
+    )
+}
