953559468e8d3302de32eda2c0c8dc337a43b77c
Aline Dubois <juillet.aline@gmail.com>
2021-05-17 20:38:58 +0200

    movie app react + css

---
+++ movie-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {MoviesList} from "./components/MoviesList";
+
+function App() {
+    return (
+        <div className="App">
+            <MoviesList/>
+        </div>
+    );
+}
+
+export default App;
