595e0cc7b659be79286d7dda7741e357bb92e586
Aline Dubois <juillet.aline@gmail.com>
2021-05-17 20:33:36 +0200

    movie app react + css

--- movie-app/src/components/Movie.js
+++ movie-app/src/components/Movie.js
@@ -1,2 +1,49 @@
 // https://api.themoviedb.org/3/movie/{movie_id}?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR
 // http://image.tmdb.org/t/p/w200/{poster_path}
+
+import "./Movie.css";
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    IconButton,
+    makeStyles,
+    Typography
+} from "@material-ui/core";
+import {Share} from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        maxWidth: 400,
+    },
+    media: {
+        width: '400px',
+        height: '500px',
+    }
+}));
+
+export const Movie = ({adult, title, overview, poster_path, release_date}) => {
+
+    const classes=useStyles();
+
+    return(
+        <Card className={classes.root}>
+            <CardHeader className="header"
+                title={title}
+                subheader={release_date}
+            />
+            <CardMedia
+                className={classes.media}
+                image= {`http://image.tmdb.org/t/p/w200/${poster_path}`}
+                title={title}
+            />
+            <CardActions disableSpacing>
+                <IconButton aria-label="share">
+                    <Share/>
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
--- movie-app/src/components/MoviesList.js
+++ movie-app/src/components/MoviesList.js
@@ -2,3 +2,47 @@
 // A venir : https://api.themoviedb.org/3/movie/upcoming?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR
 // Les mieux notés : https://api.themoviedb.org/3/movie/top_rated?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR
 // Les plus populaires : https://api.themoviedb.org/3/movie/popular?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR
+
+import "./MoviesList.css";
+
+import {useEffect, useState} from "react";
+import {CircularProgress} from "@material-ui/core";
+import {Movie} from "./Movie";
+
+export const MoviesList = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [moviesList, setMoviesList] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR")
+            .then((res) => res.json())
+            .then((res) => setMoviesList(res.results))
+            .catch(err => console.error(err))
+            .finally(() => setIsLoading(false));
+    }, [])
+
+    return (
+        <>
+
+            <div className="Movies-list">Films bientôt sur vos écrans...</div>
+
+            <div className="movies-list">
+
+                {isLoading === true ? <CircularProgress/> :
+                    moviesList.map((movie, index) => <Movie
+                            title={movie.title}
+                            adult={movie.adult}
+                            overview={movie.overview}
+                            poster_path={movie.poster_path}
+                            release_date={movie.release_date}
+                        />
+                    )
+                }
+
+
+            </div>
+        </>
+
+    )
+}
