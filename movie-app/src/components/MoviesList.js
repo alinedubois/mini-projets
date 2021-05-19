b6b90b97b3fdaafea5e220548c492acdecbdb169
Aline Dubois <juillet.aline@gmail.com>
2021-05-19 19:02:09 +0200

    ajout d'un filtre de recherche

--- movie-app/src/components/MoviesList.js
+++ movie-app/src/components/MoviesList.js
@@ -6,13 +6,14 @@
 import "./MoviesList.css";
 
 import {useEffect, useState} from "react";
-import {CircularProgress} from "@material-ui/core";
+import {CircularProgress, TextField} from "@material-ui/core";
 import {Movie} from "./Movie";
 
 export const MoviesList = () => {
 
     const [isLoading, setIsLoading] = useState(false);
     const [moviesList, setMoviesList] = useState([]);
+    const [recherche, setRecherche] = useState("");
 
     useEffect(() => {
         fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=9a85ac8036d128316c05ce13556ac979&language=fr-FR")
@@ -23,26 +24,44 @@ export const MoviesList = () => {
     }, [])
 
     return (
-        <>
+        <div className="proposition-films">
 
             <div className="Movies-list">Films bientôt sur vos écrans...</div>
 
+            <TextField label="Recherche"
+                       value={recherche}
+                       onChange={(event) => setRecherche(event.target.value)}
+            />
+
             <div className="movies-list">
 
-                {isLoading === true ? <CircularProgress/> :
-                    moviesList.map((movie, index) => <Movie
-                            title={movie.title}
-                            adult={movie.adult}
-                            overview={movie.overview}
-                            poster_path={movie.poster_path}
-                            release_date={movie.release_date}
-                        />
-                    )
+                {isLoading === true ? <CircularProgress/> : (recherche !== "" ?
+
+                        moviesList
+                            .filter((movie) => movie.title.toUpperCase().includes(recherche.toUpperCase()))
+                            .map((movie, index) => <Movie
+                                    title={movie.title}
+                                    adult={movie.adult}
+                                    overview={movie.overview}
+                                    poster_path={movie.poster_path}
+                                    release_date={movie.release_date}
+                                />
+                            ) :
+                        moviesList.map((movie, index) => <Movie
+                                title={movie.title}
+                                adult={movie.adult}
+                                overview={movie.overview}
+                                poster_path={movie.poster_path}
+                                release_date={movie.release_date}
+                            />
+                        )
+                )
+
                 }
 
 
             </div>
-        </>
+        </div>
 
     )
 }
