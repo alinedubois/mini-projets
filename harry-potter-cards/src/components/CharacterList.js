e4edb2090981fb5417a42c5fa94758c72a5f6989
Aline Dubois <juillet.aline@gmail.com>
2021-05-20 19:15:21 +0200

    création fonction clear de la recherche

--- harry-potter-cards/src/components/CharacterList.js
+++ harry-potter-cards/src/components/CharacterList.js
@@ -3,6 +3,7 @@ import "./CharacterList.css";
 import {useEffect, useState} from "react";
 import {CircularProgress, TextField} from "@material-ui/core";
 import {Character} from "./Character";
+import ClearIcon from '@material-ui/icons/Clear';
 
 export const CharacterList = () => {
     const [characters, setCharacters] = useState([]);
@@ -22,38 +23,47 @@ export const CharacterList = () => {
         <>
             <h1 className="title-page">Personnages de Harry Potter</h1>
 
-            <TextField label="Recherche"
-                       value={recherche}
-                       onChange={(event) => setRecherche(event.target.value)}
-            />
+            <div className="gestion-recherche">
+
+                <TextField label="Recherche"
+                           value={recherche}
+                           onChange={(event) => setRecherche(event.target.value)}
+                />
+
+                {recherche !== "" &&  <ClearIcon
+
+                    onClick={() => setRecherche("")}
+                />}
+
+            </div>
 
             <div className="CharacterList">
                 {isLoading === true ? <CircularProgress/> : (recherche !== "" ?
                         characters
                             .filter((character) => character.name.toLowerCase().includes(recherche.toLowerCase()))
                             .map((character, index) => <Character
-                                key={index}
-                                name={character.name}
-                                image={character.image}
-                                actor={character.actor}
-                                ancestry={character.ancestry}
-                                dateOfBirth={character.dateOfBirth}
-                                house={character.house}
-                                patronus={character.patronus}
-                            />
-                ) :
-                    characters
-                    .map((character, index) => <Character
-                    key={index}
-                    name={character.name}
-                    image={character.image}
-                    actor={character.actor}
-                    ancestry={character.ancestry}
-                    dateOfBirth={character.dateOfBirth}
-                    house={character.house}
-                    patronus={character.patronus}
-                    />
-                    )
+                                    key={index}
+                                    name={character.name}
+                                    image={character.image}
+                                    actor={character.actor}
+                                    ancestry={character.ancestry}
+                                    dateOfBirth={character.dateOfBirth}
+                                    house={character.house}
+                                    patronus={character.patronus}
+                                />
+                            ) :
+                        characters
+                            .map((character, index) => <Character
+                                    key={index}
+                                    name={character.name}
+                                    image={character.image}
+                                    actor={character.actor}
+                                    ancestry={character.ancestry}
+                                    dateOfBirth={character.dateOfBirth}
+                                    house={character.house}
+                                    patronus={character.patronus}
+                                />
+                            )
                 )
                 }
             </div>
