c2d6796c8f5c6db5bbe44c50edafed3e95ab3fe0
Aline Dubois <juillet.aline@gmail.com>
2021-05-23 19:45:44 +0200

    creation de la structure de la page et mise en place du fetch

--- jack-bauer-jokes/src/components/Joke.js
+++ jack-bauer-jokes/src/components/Joke.js
@@ -1,1 +1,40 @@
 // http://api.icndb.com/jokes/random?firstName=Jack&lastName=Bauer
+import "./Joke.css";
+import {Button} from "@material-ui/core";
+import {useEffect, useState} from "react";
+
+export const Joke = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+    const [joke, setJoke] = useState("");
+
+    useEffect(() => {
+        fetch("http://api.icndb.com/jokes/random?firstName=Jack&lastName=Bauer")
+            .then((response) => response.json())
+            .then((responseJson) => {
+                    setIsLoaded(true);
+                    setJoke(responseJson.value.joke);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    },[])
+
+
+    return (
+        <div className="page-principale">
+            <div className="logo">
+                <img src="/photo/24-title.svg.png" alt="logo 24h"/>
+            </div>
+            <div className="photo-JB">
+                <img src="/photo/JaBa.jpg" alt="Jack Bauer"/>
+            </div>
+            <div className="joke">
+                {joke}
+            </div>
+            <Button className="button" variant="contained">Next joke</Button>
+        </div>
+    )
+}
